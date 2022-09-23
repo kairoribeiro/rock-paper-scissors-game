d497bdb45f02410f8cf92000621a55ffdea662b1
Kairo Ribeiro <kairo10@gmail.com>
2022-09-23 16:10:20 -0400

    add cached elem

---
+++ app.js
@@ -0,0 +1,30 @@
+/*-------------------------------- Constants --------------------------------*/
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+const userScore = document.querySelector('#user-score');
+const computerScore = document.querySelector('#computer-score');
+const scoreBoard = document.querySelector('.score-board')
+const result = document.querySelector('.result');
+const choiceRock = document.querySelector('#r');
+const choicePaper = document.querySelector('#p');
+const choiceScissors = document.querySelector('#s');
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+choiceRock.addEventListener('click', )
+choicePaper.addEventListener('click', )
+choiceScissors.addEventListener('click', )
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    
+}
