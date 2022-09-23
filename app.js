6b28ae5008f16d9a01440d83ec3d5ed1d957266f
Kairo Ribeiro <kairo10@gmail.com>
2022-09-23 17:10:28 -0400

    add functions

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
+let userScore = 0;
+let computerScore = 0;
 
 /*------------------------ Cached Element References ------------------------*/
-const userScore = document.querySelector('#user-score');
-const computerScore = document.querySelector('#computer-score');
+const userScore_span = document.querySelector('#user-score');
+const computerScore_span = document.querySelector('#computer-score');
 const scoreBoard = document.querySelector('.score-board')
-const result = document.querySelector('.result');
+const result = document.querySelector('.result > p');
 const choiceRock = document.querySelector('#r');
 const choicePaper = document.querySelector('#p');
 const choiceScissors = document.querySelector('#s');
@@ -16,9 +17,15 @@ const choiceScissors = document.querySelector('#s');
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-choiceRock.addEventListener('click', )
-choicePaper.addEventListener('click', )
-choiceScissors.addEventListener('click', )
+choiceRock.addEventListener('click', function() {
+    game('r');
+})
+choicePaper.addEventListener('click', function() {
+    game('p');
+})
+choiceScissors.addEventListener('click', function() {
+    game('s');
+})
 
 
 
@@ -26,5 +33,56 @@ choiceScissors.addEventListener('click', )
 
 function getComputerChoice() {
     const choices = ['r', 'p', 's'];
-    
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber]; // randonNumber works as the index
+}
+
+function convertToWord(letter) {
+    if (letter === "r") {
+        return "Rock";
+    }
+    if (letter === "p") {
+        return "Paper";
+    } else {
+         return "Scissor";
+    }
+
+}
+
+function userWins(userChoice, computerChoice) {
+    userScore ++;
+    userScore_span.innerHTML = userScore;
+    result.innerHTML = convertToWord(userChoice)  + ' beats ' + convertToWord(computerChoice)  + ' 👨‍💻 User Wins 🔥';
+}
+
+function computerWins(userChoice, computerChoice) {
+    computerScore ++;
+    computerScore_span.innerHTML = computerScore;
+    result.innerHTML = convertToWord(computerChoice)  + ' beats ' + convertToWord(userChoice)  +' 💻 Computer Wins';
+}
+
+function draw() {
+    result.innerHTML = "It's a draw";
+}
+
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            userWins(userChoice, computerChoice);
+            break;
+        case "sr":
+        case "rp":
+        case "ps":
+            computerWins(userChoice, computerChoice);
+            break;
+        case "ss":
+        case "rr":
+        case "pp":
+            draw(userChoice, computerChoice);
+            break;
+    }
 }
